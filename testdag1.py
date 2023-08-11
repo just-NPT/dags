8602984761a4d42223e244c43993de2b957de6a5
Nguyễn Phương Trung <59968811+just-NPT@users.noreply.github.com>
2023-08-11 11:40:07 +0700

    Add files via upload

---
+++ testdag1.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+def print_hello():
+    return "Hello Airflow!"
+
+dag = DAG(
+    'simple_dag',
+    default_args={
+        'owner': 'airflow',
+        'start_date': datetime.utcnow(),
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='A simple example DAG',
+)
+
+task1 = PythonOperator(
+    task_id='print_hello_task',
+    python_callable=print_hello,
+    dag=dag,
+)
